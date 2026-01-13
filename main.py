fc21a1666609d66d62b1dc545e115685aab191f4
Felipe Ribeiro Rodrigues dos Santos <feliperrsdev@gmail.com>
2026-01-13 15:24:19 -0300

    implemented load_categories(), save_categories(), add_new_categories() and add_new_expense()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 
 def main_menu():
@@ -12,9 +13,54 @@ def main_menu():
     return choice
 
 
+def load_categories():
+    try:
+        with open("categories.json", "r") as file:
+            return set(json.load(file)) # return saved list as a set
+    except FileNotFoundError:
+        with open("categories.json", "w") as file:
+            json.dump([], file) # creates a new categories.json with empty list
+        return set()    # return an empty set if there is no categories.json
+    except json.decoder.JSONDecodeError:
+        raise ValueError(
+            "categories.json exists but contains invalid JSON. "
+        )
+
+def save_categories(categories: set):   # receive a set from add_new_category()
+    with open("categories.json", "w") as file:
+        json.dump(sorted(categories), file, indent=2) # save the set as a sorted list
+
+def add_new_category():
+    categories = load_categories()
+    
+    print("Type the new category:")
+    new_category = input(">>> ").strip().lower()
+
+    if not new_category:
+        print("Category cannot be empty!\n")
+        return
+    if new_category in categories:
+        print("This category already exists.\n")
+        return
+    
+    categories.add(new_category)  # add new category to "copy" set
+    save_categories(categories) # send the "copy" set to save_categories()
+    
+
 
 def add_new_expense():
-    pass
+    print("# Adding a new expense #")
+    print("Choose a category:")
+    print("CATEGORY LIST")
+    print("0 - Add new category")
+    choice = int(input(">>> "))
+    if choice == 0:
+        add_new_category()
+    date = datetime.today().strftime('%Y-%m-%d')
+    new_expense = {
+        "date" : date
+    }
+    print(new_expense)
 
 def view_all_expenses():
     pass
