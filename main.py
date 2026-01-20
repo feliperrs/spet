0cf4e1d3fca43f80767f623e534ff4048283dcab
Felipe Ribeiro Rodrigues dos Santos <feliperrsdev@gmail.com>
2026-01-20 15:13:20 -0300

    implemented list_filter_categories(), select_filter_categories(), filter_by_category(), filter_by_payment_method(), filter_by_date(), filter_expenses(), select_month_report(), monthly_report_summary(), category_breakdown() and payment_method_breakdown()

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def add_new_category():
 
 def list_categories():
     categories = load_categories()
-    print("Choose a category:")
+    print("\nChoose a category:")
     print("0 - Add new category")
     for index, category in enumerate(sorted(categories), start=1):
         print(f"{index} - {category.title()}")
@@ -63,7 +63,7 @@ def select_category():
     while True:
         categories = sorted(load_categories())
         try:
-            choice = int(input(">>> "))
+            choice = int(input("\n>>> "))
         except ValueError:
             print(f"\nSelect a NUMBER from 0 to {len(categories)}\n")
             continue
@@ -173,11 +173,180 @@ def view_all_expenses():
         print(f"Payment Method: {expense['payment_method']}")
         print(f"Date: {expense['date']}\n")
 
-def filter_expenses():
-    pass
+# ----------------------------- FILTER EXPENESES ----------------------------- #
 
+def list_filter_categories():
+    categories = load_categories()
+    print("\nChoose a category:")
+    for index, category in enumerate(sorted(categories), start=1):
+        print(f"{index} - {category.title()}")
+
+def select_filter_categories():
+    while True:
+        categories = sorted(load_categories())
+        try:
+            choice = int(input("\n>>> "))
+        except ValueError:
+            print(f"\nSelect a NUMBER from 0 to {len(categories)}\n")
+            continue
+        else:
+            if 1 <= choice <= len(categories):
+                selected_category = categories[choice - 1]
+            else:
+                print(f"\nSelect from 1 to {len(categories)}\n")
+                continue
+        return selected_category
+
+def filter_by_category():
+    list_filter_categories()
+    category = select_filter_categories()
+    expenses_list = load_expenses()
+    for expense in expenses_list:
+        if category == expense["category"]:
+            print(f"\nDescription: {expense['description']}")
+            print(f"Amount: ${expense['amount']}")
+            print(f"Category: {expense['category']}")
+            print(f"Payment Method: {expense['payment_method']}")
+            print(f"Date: {expense['date']}\n")
+
+def filter_by_payment_method():
+    selected_payment_method = select_payment_method()
+    expenses_list = load_expenses()
+    for payment in expenses_list:
+        if selected_payment_method == payment["payment_method"]:
+            print(f"\nDescription: {payment['description']}")
+            print(f"Amount: ${payment['amount']}")
+            print(f"Category: {payment['category']}")
+            print(f"Payment Method: {payment['payment_method']}")
+            print(f"Date: {payment['date']}\n")
+
+def filter_by_date():
+    expenses_list = load_expenses()
+    date_list = []
+    for expense in expenses_list:
+        if expense['date'][:7] not in date_list:
+            date_list.append(expense['date'][:7])
+    print("\nSelect a MONTH to filter:")
+    for index, month in enumerate(date_list, start=1):
+        print(f"{index} - {month}")
+    month_choice = int(input("\n>>> "))
+    if 1 <= month_choice <= len(date_list):
+        selected_month = date_list[month_choice - 1]
+        for expense in expenses_list:
+            if expense['date'][:7] == selected_month:
+                print(f"\nDescription: {expense['description']}")
+                print(f"Amount: ${expense['amount']}")
+                print(f"Category: {expense['category']}")
+                print(f"Payment Method: {expense['payment_method']}")
+                print(f"Date: {expense['date']}\n")
+    
+    
+def filter_expenses():
+    cls()
+    print("How do you want to filter?\n")
+    print("1 - By CATEGORY")
+    print("2 - By PAYMENT METHOD")
+    print("3 - By DATE\n")
+    user_choice = int(input(">>> "))
+    if user_choice == 1:
+        filter_by_category()
+    elif user_choice == 2:
+        filter_by_payment_method()
+    elif user_choice == 3:
+        filter_by_date()
+    
+# ---------------------------------- REPORT ---------------------------------- #
+
+def select_month_report():
+    cls()
+    expenses_list = load_expenses()
+    date_list = []
+    for expense in expenses_list:
+        if expense['date'][:7] not in date_list:
+            date_list.append(expense['date'][:7])
+    print("\nSelect a MONTH to check the report:")
+    for index, month in enumerate(date_list, start=1):
+        print(f"{index} - {month}")
+    month_choice = int(input("\n>>> "))
+    if 1 <= month_choice <= len(date_list):
+        selected_month = date_list[month_choice - 1]
+    return selected_month
+
+def monthly_report_summary(selected_month):
+    cls()
+    print(f"MONTHLY SUMMARY FOR {selected_month}\n")
+    expenses_list = load_expenses()
+    total_spent = 0
+    total_transactions = 0
+    for expense in expenses_list:
+        if expense['date'][:7] == selected_month:
+            total_spent += expense['amount']
+            total_transactions += 1
+    average_expense = total_spent / total_transactions
+    
+    print(f"Total spent: ${total_spent}")
+    print(f"Transactions: {total_transactions}")
+    print(f"Average expense: ${average_expense}")
+
+def category_breakdown(selected_month):
+    expenses_list = load_expenses()
+    
+    total_spent = 0
+    category_totals = {}
+    
+    for expense in expenses_list:
+        if expense['date'][:7] == selected_month:
+            total_spent += expense['amount']
+            category = expense['category']
+            amount = expense['amount']
+            if expense['category'] not in category_totals:
+                    category_totals[category] = 0
+            category_totals[category] += amount
+            
+    print("\nCategory Breakdown:")
+    category_percentages = {}
+    for category, amount in category_totals.items():
+        percentage = (amount / total_spent) * 100
+        category_percentages[category] = percentage
+    
+    for category in category_totals:
+        amount = category_totals[category]
+        percentage = category_percentages[category]
+        print(f"- {category.title()} : ${amount:.2f} ({percentage:.2f}%)")
+    
+    
+def payment_method_breakdown(selected_month):
+    expenses_list = load_expenses()
+    
+    total_spent = 0
+    paymenth_method_totals = {}
+    
+    for expense in expenses_list:
+        if expense['date'][:7]== selected_month:
+            total_spent += expense['amount']
+            payment_method = expense['payment_method']
+            amount = expense['amount']
+            if expense['payment_method'] not in paymenth_method_totals:
+                paymenth_method_totals[payment_method] = 0
+            paymenth_method_totals[payment_method] += amount
+    
+    print(f"\nPayment Method Breakdown:")
+    payment_method_percentages = {}
+    for method, amount in paymenth_method_totals.items():
+        percentage = (amount / total_spent) * 100
+        payment_method_percentages[method] = percentage
+    
+    for method in paymenth_method_totals:
+        amount = paymenth_method_totals[method]
+        percentage = payment_method_percentages[method]
+        print(f"- {method.title()} : ${amount:.2f} ({percentage:.2f}%)")
+        
+        
 def monthly_report():
-    pass
+    selected_month = select_month_report()
+    monthly_report_summary(selected_month)
+    category_breakdown(selected_month)
+    payment_method_breakdown(selected_month)
 
 # ----------------------------------- MAIN ----------------------------------- #
 
