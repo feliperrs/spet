944047257d8818cf3061574d3c9cb2369e5dc2cd
Felipe Ribeiro Rodrigues dos Santos <feliperrsdev@gmail.com>
2026-01-21 18:26:09 -0300

    refactor user prompts and input handling for improved clarity and error management

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ cls = lambda: os.system('cls') # cleans console
 
 
 def main_menu():
-    print("\nWhat do you want to do ?")
+    print("What do you want to do ?")
     print("1 - Add a new expense")
     print("2 - View all expenses")
     print("3 - Filter expenses")
@@ -54,7 +54,7 @@ def add_new_category():
 
 def list_categories():
     categories = load_categories()
-    print("\nChoose a category:")
+    print("Choose a category:")
     print("0 - Add new category")
     for index, category in enumerate(sorted(categories), start=1):
         print(f"{index} - {category.title()}")
@@ -63,9 +63,11 @@ def select_category():
     while True:
         categories = sorted(load_categories())
         try:
+            list_categories()
             choice = int(input("\n>>> "))
         except ValueError:
-            print(f"\nSelect a NUMBER from 0 to {len(categories)}\n")
+            cls()
+            print(f"Select a NUMBER from 0 to {len(categories)}\n")
             continue
         else:
             if choice == 0:
@@ -73,51 +75,59 @@ def select_category():
             elif 1 <= choice <= len(categories):
                 selected_category = categories[choice - 1]
             else:
-                print(f"\nSelect from 0 to {len(categories)}\n")
+                cls()
+                print(f"Select from 0 to {len(categories)}\n")
                 continue
         return selected_category
 
 # ---------------------------------- AMOUNT ---------------------------------- #
 
 def select_amount():
+    cls()
     while True:
-        print("\nEnter the amount")
+        print("Enter the amount")
         try:
             amount = float(input("\n>>> "))
         except ValueError:
-            print("\nInsert a number")
+            cls()
+            print("Insert a number\n")
             continue
         return amount
 
 # -------------------------------- DESCRIPTION ------------------------------- #
 
 def select_description():
+    cls()
     while True:
-        print("\nEnter a short description")
+        print("Enter a short description")
         description = input("\n>>> ")
         if not description:
-            print("\nYou must enter a short description")
+            cls()
+            print("You must enter a short description\n")
             continue
         return description
 
 # ------------------------------ PAYMENT METHOD ------------------------------ #
 
 def select_payment_method():
+    cls()
     while True:
-        print("\nSelect the payment method:")
+        print("Select the payment method:")
         payment_methods_list = ["cash", "credit", "debit"]
         for index, choice in enumerate(payment_methods_list, start=1):
             print(f"{index} - {choice.title()}")
         try:
             choice = int(input("\n>>> "))
         except ValueError:
-            print(f"\nYou must enter a NUMBER from 1 to {len(payment_methods_list)}")
+            cls()
+            print(f"You must enter a NUMBER from 1 to {len(payment_methods_list)}\n")
             continue
         else:
             if 1 <= choice <= len(payment_methods_list):
                 selected_payment_method = payment_methods_list[choice - 1]
             else:
-                print(f"\nSelect an option from 1 to {len(payment_methods_list)}")
+                cls()
+                print(f"Select an option from 1 to {len(payment_methods_list)}\n")
                 continue
         return selected_payment_method
         
@@ -144,8 +154,6 @@ def save_new_expense(expense):
     
     
 def add_new_expense():
-    print("\n# Adding a new expense #\n")
-    list_categories()
     category = select_category()
     amount = select_amount()
     description = select_description()
@@ -159,25 +167,32 @@ def add_new_expense():
         "payment_method" : payment_method 
     }
     save_new_expense(new_expense)
-    print(new_expense)
+    cls()
+    print("# Adding a new expense #")
+    print("\nYour expense was added !")
+    print(f"Description: {new_expense['description'].capitalize()}")
+    print(f"Amount: ${new_expense['amount']:.2f}")
+    print(f"Category: {new_expense['category'].title()}")
+    print(f"Payment Method: {new_expense['payment_method'].title()}")
 
 # ------------------------------- VIEW EXPENSES ------------------------------ #
 
 def view_all_expenses():
     expenses_list = load_expenses()
     cls()
+    print("List of Expenses:\n")
     for expense in expenses_list:
         print(f"Description: {expense['description']}")
-        print(f"Amount: ${expense['amount']}")
-        print(f"Category: {expense['category']}")
-        print(f"Payment Method: {expense['payment_method']}")
+        print(f"Amount: ${expense['amount']:.2f}")
+        print(f"Category: {expense['category'].capitalize()}")
+        print(f"Payment Method: {expense['payment_method'].title()}")
         print(f"Date: {expense['date']}\n")
 
 # ----------------------------- FILTER EXPENESES ----------------------------- #
 
 def list_filter_categories():
     categories = load_categories()
-    print("\nChoose a category:")
+    print("Choose a category:")
     for index, category in enumerate(sorted(categories), start=1):
         print(f"{index} - {category.title()}")
 
@@ -187,26 +202,30 @@ def select_filter_categories():
         try:
             choice = int(input("\n>>> "))
         except ValueError:
+            cls()
             print(f"\nSelect a NUMBER from 0 to {len(categories)}\n")
             continue
         else:
             if 1 <= choice <= len(categories):
                 selected_category = categories[choice - 1]
             else:
+                cls()
                 print(f"\nSelect from 1 to {len(categories)}\n")
                 continue
         return selected_category
 
 def filter_by_category():
+    cls()
     list_filter_categories()
     category = select_filter_categories()
     expenses_list = load_expenses()
+    cls()
     for expense in expenses_list:
         if category == expense["category"]:
-            print(f"\nDescription: {expense['description']}")
-            print(f"Amount: ${expense['amount']}")
-            print(f"Category: {expense['category']}")
-            print(f"Payment Method: {expense['payment_method']}")
+            print(f"\nDescription: {expense['description'].capitalize()}")
+            print(f"Amount: ${expense['amount']:.2f}")
+            print(f"Category: {expense['category'].title()}")
+            print(f"Payment Method: {expense['payment_method'].title()}")
             print(f"Date: {expense['date']}\n")
 
 def filter_by_payment_method():
@@ -214,63 +233,96 @@ def filter_by_payment_method():
     expenses_list = load_expenses()
     for payment in expenses_list:
         if selected_payment_method == payment["payment_method"]:
-            print(f"\nDescription: {payment['description']}")
-            print(f"Amount: ${payment['amount']}")
-            print(f"Category: {payment['category']}")
-            print(f"Payment Method: {payment['payment_method']}")
+            print(f"\nDescription: {payment['description'].capitalize()}")
+            print(f"Amount: ${payment['amount']:.2f}")
+            print(f"Category: {payment['category'].title()}")
+            print(f"Payment Method: {payment['payment_method'].title()}")
             print(f"Date: {payment['date']}\n")
 
 def filter_by_date():
-    expenses_list = load_expenses()
-    date_list = []
-    for expense in expenses_list:
-        if expense['date'][:7] not in date_list:
-            date_list.append(expense['date'][:7])
-    print("\nSelect a MONTH to filter:")
-    for index, month in enumerate(date_list, start=1):
-        print(f"{index} - {month}")
-    month_choice = int(input("\n>>> "))
-    if 1 <= month_choice <= len(date_list):
-        selected_month = date_list[month_choice - 1]
+    cls()
+    while True:
+        expenses_list = load_expenses()
+        date_list = []
         for expense in expenses_list:
-            if expense['date'][:7] == selected_month:
-                print(f"\nDescription: {expense['description']}")
-                print(f"Amount: ${expense['amount']}")
-                print(f"Category: {expense['category']}")
-                print(f"Payment Method: {expense['payment_method']}")
-                print(f"Date: {expense['date']}\n")
+            if expense['date'][:7] not in date_list:
+                date_list.append(expense['date'][:7])
+        print("Select a MONTH to filter:")
+        for index, month in enumerate(date_list, start=1):
+            print(f"{index} - {month}")
+        try:
+            month_choice = int(input("\n>>> "))
+        except ValueError:
+            cls()
+            print(f"Select a NUMBER from 1 to {len(date_list)}\n")
+            continue
+        if 1 <= month_choice <= len(date_list):
+            selected_month = date_list[month_choice - 1]
+            for expense in expenses_list:
+                if expense['date'][:7] == selected_month:
+                    print(f"\nDescription: {expense['description'].capitalize()}")
+                    print(f"Amount: ${expense['amount']:.2f}")
+                    print(f"Category: {expense['category'].title()}")
+                    print(f"Payment Method: {expense['payment_method'].title()}")
+                    print(f"Date: {expense['date']}\n")
+        else:
+            cls()
+            print(f"Select from 1 to {len(date_list)}\n")
+            continue
     
     
 def filter_expenses():
-    cls()
-    print("How do you want to filter?\n")
-    print("1 - By CATEGORY")
-    print("2 - By PAYMENT METHOD")
-    print("3 - By DATE\n")
-    user_choice = int(input(">>> "))
-    if user_choice == 1:
-        filter_by_category()
-    elif user_choice == 2:
-        filter_by_payment_method()
-    elif user_choice == 3:
-        filter_by_date()
+    while True:
+        print("How do you want to filter?\n")
+        print("1 - By CATEGORY")
+        print("2 - By PAYMENT METHOD")
+        print("3 - By DATE")
+        print("0 - BACK")
+        try:
+            user_choice = int(input("\n>>> "))
+        except ValueError:
+            cls()
+            print("Select a NUMBER from 1 to 3\n")
+            continue
+        if user_choice == 1:
+            filter_by_category()
+        elif user_choice == 2:
+            filter_by_payment_method()
+        elif user_choice == 3:
+            filter_by_date()
+        elif user_choice == 0:
+            cls()
+            break
+        else:
+            cls()
+            print("Select from 1 to 3\n")
     
 # ---------------------------------- REPORT ---------------------------------- #
 
 def select_month_report():
     cls()
-    expenses_list = load_expenses()
-    date_list = []
-    for expense in expenses_list:
-        if expense['date'][:7] not in date_list:
-            date_list.append(expense['date'][:7])
-    print("\nSelect a MONTH to check the report:")
-    for index, month in enumerate(date_list, start=1):
-        print(f"{index} - {month}")
-    month_choice = int(input("\n>>> "))
-    if 1 <= month_choice <= len(date_list):
-        selected_month = date_list[month_choice - 1]
-    return selected_month
+    while True:
+        expenses_list = load_expenses()
+        date_list = []
+        for expense in expenses_list:
+            if expense['date'][:7] not in date_list:
+                date_list.append(expense['date'][:7])
+        print("Select a MONTH to check the report:")
+        for index, month in enumerate(date_list, start=1):
+            print(f"{index} - {month}")
+        try:
+            month_choice = int(input("\n>>> "))
+        except ValueError:
+            cls()
+            print(f"Select a NUMBER from 1 to {len(date_list)}\n")
+            continue
+        if 1 <= month_choice <= len(date_list):
+            selected_month = date_list[month_choice - 1]
+        else:
+            cls()
+            print(f"Select from 1 to {len(date_list)}\n")
+            continue
+        return selected_month
 
 def monthly_report_summary(selected_month):
     cls()
@@ -347,12 +399,14 @@ def monthly_report():
     monthly_report_summary(selected_month)
     category_breakdown(selected_month)
     payment_method_breakdown(selected_month)
+    print("\n")
 
 # ----------------------------------- MAIN ----------------------------------- #
 
 while True:
     try:
         user_choice = main_menu()
+        cls()
     except ValueError:
         print("\nYou should enter a number !\n")
     else:
